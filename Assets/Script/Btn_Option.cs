7246e62c1ed523e33f7308f78be8bda2e78ed3a2
SGYT1008 <3095458189@qq.com>
2018-03-06 10:18:35 +0800

    添加选项事件

---
+++ Assets/Script/Btn_Option.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Btn_Option : MonoBehaviour {
+
+    public Button btnOption;
+    private GameObject Instance;
+	// Use this for initialization
+	void Start () {
+        btnOption.GetComponent<Button>().onClick.AddListener(() =>{ OnClickOption(); });
+   
+    }
+
+    public void OnClickOption()
+    {
+        GameObject obj = Instantiate(Resources.Load("Prefab/UI/StartUI_Option")) as GameObject;
+      
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
