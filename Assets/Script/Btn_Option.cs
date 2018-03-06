d97d13453fdc369c3ca2f6fd89c2fede39c419e4
sgyt1008 <3095458189@qq.com>
2018-03-07 00:12:32 +0800

    预制体修改，选关界面搭建

--- Assets/Script/Btn_Option.cs
+++ Assets/Script/Btn_Option.cs
@@ -4,24 +4,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Btn_Option : MonoBehaviour {
+public class Btn_Option : MonoBehaviour
+{
 
-    public Button btnOption;
+    private  GameObject Start_option;
+    public GameObject Start_UI;
+    public GameObject Root;
+    private Button btnStartUIOption;
+    private Button btnStartOptionReturn;
     private GameObject Instance;
-	// Use this for initialization
-	void Start () {
-        btnOption.GetComponent<Button>().onClick.AddListener(() =>{ OnClickOption(); });
-   
+    private bool StartOptionPanelExist=false;
+    // Use this for initialization
+    void Start()
+    {
+        Start_UI.transform.SetParent(Root.transform);
+        btnStartUIOption = Start_UI.transform.Find("bg/Btn_Options").GetComponent<Button>();
+        btnStartUIOption.onClick.AddListener(OnClickOption);
+
+
     }
 
     public void OnClickOption()
     {
-        GameObject obj = Instantiate(Resources.Load("Prefab/UI/StartUI_Option")) as GameObject;
-      
+ 
+        if (!StartOptionPanelExist)
+        {
+            Start_option = Instantiate(Resources.Load("Prefab/UI/StartUI_Option")) as GameObject;
+            Start_option.transform.SetParent(Root.transform,false);
+            StartOptionPanelExist = true;
+        }
+        if (Start_option!=null)
+        {
+            Start_option.SetActive(true);
+        }
+        btnStartOptionReturn = Start_option.transform.Find("bg/btnReturn").GetComponent<Button>();
+        btnStartOptionReturn.onClick.AddListener(OnClickReturn);
+        Start_UI.SetActive(false);
+
+    }
+
+    private void OnClickReturn()
+    {
+        Start_UI.SetActive(true);
+        Start_option.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 }
