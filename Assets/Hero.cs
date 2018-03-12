246e9afe540dc1e23ee879b1dda1f14efc8aeb22
sgyt1008 <3095458189@qq.com>
2018-03-13 00:05:12 +0800

    人物动画的完成

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -23,17 +23,30 @@ public class Hero : MonoBehaviour
     }
     public GameObject objColl;
     public  GameObject objTri;
-    // Use this for initialization
+
+    private float speed;
+    Rigidbody2D rigid;
+    Animator animator;
     void Start()
     {
-
+       rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        PlayerRun();
     }
+
+  //  private void LateUpdate()
+  //  {
+  //      if (gameObject.transform.Find("Main Camera").gameObject != null)
+  //      {
+  //          this.transform.LookAt(this.gameObject.transform.position);
+  //      }
+  //  }
     void OnCollisionEnter2D(Collision2D other)
     {
 
@@ -49,5 +62,28 @@ public class Hero : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         CollisionCondition.Instance.objTriExit = other.gameObject;
+    }
+
+    private void PlayerRun()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //向前运动
+            Debug.Log("按下D建");
+            //transform.position = 
+            rigid.velocity = new Vector2(2,rigid.velocity.y);
+            animator.SetBool("Run",true);
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //向后运动
+            Debug.Log("按下A建");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //向上运动
+            Debug.Log("按下空格");
+        }
     }
 }
