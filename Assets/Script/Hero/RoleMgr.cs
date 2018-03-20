0e9d0cf973111f2fa70bfceb634c059763c4ed93
yuting <735840049@qq.com>
2018-03-20 18:15:18 +0800

    界面

---
+++ Assets/Script/Hero/RoleMgr.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class RoleMgr : Singleton<RoleMgr>
+{
+    public int Money { get; internal set; }
+    public int Diamond { get; set; }
+}
